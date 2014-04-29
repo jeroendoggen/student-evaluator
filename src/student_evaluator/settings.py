ae5468385c0895729f3699f406cba37706af8c2d
Jeroen Doggen <jeroendoggen@gmail.com>
2014-04-29 17:41:01 +0200

    read cli arguments

--- src/student_evaluator/settings.py
+++ src/student_evaluator/settings.py
@@ -6,7 +6,7 @@
 
 import os
 import ConfigParser
-
+import argparse
 
 class Settings:
     """ Contains all the tools to analyse Blackboard assignments """
@@ -21,9 +21,17 @@ class Settings:
     working_dir = os.getcwd()
     folders_list = []
     subfolders_list = []
+    students = 0
+
+    parser = argparse.ArgumentParser(
+        prog="student_evaluator",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="student_evaluator commandline arguments:",
+        epilog="Report bugs to jeroendoggen@gmail.com.")
 
     def __init__(self):
         self.read_config_file(self.config_file)
+        self.cli_arguments()
 
     def config_section_map(self, section):
         """ Helper function to read config settings """
@@ -53,3 +61,24 @@ class Settings:
             #TODO: this does not work!! (AttributeError or KeyError needed? both?)
             print("Error while processing config file")
             sys.exit(1)
+
+    def cli_arguments(self):
+        """Configure a read all the cli arguments."""
+        self.configure_cli_arguments()
+        self.get_cli_arguments()
+
+    def configure_cli_arguments(self):
+        """Configure all the cli arguments."""
+        self.parser.add_argument("-s", metavar="students",
+          help="Number of user accounts to create for the students")
+        self.parser.add_argument('-l', metavar='logfile',
+          help="Set the name the logfile to use")
+
+    def get_cli_arguments(self):
+        """Read all the cli arguments."""
+        args = self.parser.parse_args()
+        if (args.s is not None):
+            self.students = args.s
+        if (args.l is not None):
+            self.logfile = args.l
+
