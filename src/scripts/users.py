f3442ab2f256dd6a87be6e31385268f036f0c0ff
Jeroen Doggen <jeroendoggen@gmail.com>
2014-05-05 09:08:47 +0200

    Added script to add multiple users to a system

---
+++ src/scripts/users.py
@@ -0,0 +1,26 @@
+"""
+    Student Evaluator: add users
+    Copyright 2014, Jeroen Doggen, jeroendoggen@gmail.com
+"""
+
+from __future__ import print_function, division  # We require Python 2.6+
+
+import sys
+import os
+
+number = 5
+username_prefix = "s"
+
+def run(number):
+    """Create user accounts on the system """
+    os.system("addgroup students")
+    for x in range(0, number):
+        username = username_prefix + str(x)
+        print(username)
+        # password: 'student'
+        os.system("useradd -p  SNSr7/A1BYa12 -g students " + username )
+        os.system("mkdir /home/" + username)
+        os.system("chown "+ username + ":students " + "/home/" + username )
+
+if __name__ == "__main__":
+    sys.exit(run(number))
