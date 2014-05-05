ad4855b57553293369a0b6111a294cb794f2fcf6
Jeroen Doggen <jeroendoggen@gmail.com>
2014-05-05 14:56:19 +0200

    Created (&uploaded) PyPI package

--- src/setup.py
+++ src/setup.py
@@ -10,7 +10,7 @@ from distutils.core import setup
 from student_evaluator import __version__
 
 
-with open('README.md') as readme_file:
+with open('README.txt') as readme_file:
     LONG_DESCRIPTION = readme_file.read()
 
 setup(name='student_evaluator',
